3bfe9f6bf2abfe9d32e4884c6cab5233d61e65d3
0x1 <kevin.francart3@gmail.com>
2026-08-22 15:13:25 +0200

    misc

--- src/Data/Unit.rs
+++ src/Data/Unit.rs
@@ -1,3 +1,3 @@
 pub fn Data_Unit_unit() -> crate::UnknownType {
-    crate::UnknownType::new(crate::Record_a { ..Default::default() })
+    crate::Value::Record_a(perceus_ptr::PerceusPtr::new(crate::Record_a { ..Default::default() }))
 }
