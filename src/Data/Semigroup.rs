00d9c75a8ece790563fb229a778bcfe30396359c
0x1 <kevin.francart3@gmail.com>
2026-08-30 19:23:52 +0200

    misc

--- src/Data/Semigroup.rs
+++ src/Data/Semigroup.rs
@@ -2,7 +2,7 @@ pub fn Data_Semigroup_concatString(mut a0: String, mut a1: String) -> String {
     format!("{}{}", a0, a1)
 }
 
-pub fn Data_Semigroup_concatArray(mut a0: crate::UnknownType, mut a1: crate::UnknownType) -> crate::UnknownType {
+pub fn Data_Semigroup_concatArray<a: Clone + 'static>(mut a0: crate::UnknownType, mut a1: crate::UnknownType) -> crate::UnknownType {
     let mut vec = a0.unwrap_array().to_vec();
     vec.extend(a1.unwrap_array().to_vec());
     crate::mk_array(vec)
